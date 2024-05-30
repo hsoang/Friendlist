5e7f644e9d8c6418d5199a7ddc5c01ca96f42678
hsoang <hoangle63808@yahoo.com>
2024-05-29 22:21:18 -0700

    Switch to Firebase

--- main.py
+++ main.py
@@ -1,87 +1,29 @@
-import csv
-import boto3
-import calendar
-import datetime
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
-csv_file_path = 'C:/Users/hoang/OneDrive/Documents/GitHub/Friendlist/credentials.csv'
 
-def read_aws_creds(csv_file_path):
-    with open(csv_file_path, mode='r') as file:
-        csv_reader = csv.reader(file)
-        next(csv_reader)  
-        for key in csv_reader:
-            return key[0], key[1] 
+
+cred = credentials.Certificate("C:/Users/hoang/OneDrive/Documents/GitHub/Friendlist/credentials.json")
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()
+
 
 
 def showCalendar():
     # cur_datetime = datetime.datetime.now()
     # year = cur_datetime.year
     # print(calendar.calendar(year))
-
-    user_id = "lee"
-    event_id = "d002f55b-bde4-48fc-a328-1572bb5fd6d6"
-
-    response = table.get_item(
-        Key={
-            'UserID': user_id,
-            'EventID': event_id
-        }
-    )
-
-    item = response.get('Item', None)
-
-    if item:
-        print(f"  UserID: {item['UserID']}")
-        print(f"  Date: {item['Date']}")
-        print(f"  Start Time: {item['StartTime']}")
-        print(f"  End Time: {item['EndTime']}")
-    else:
-        print("Info not found")
+    print("FIX")
     
 
 def addEvent():
-    user_id = "lee"
-    event_id = "work"
-    date = "2024-05-30"
-    start_time = "10:00:00"
-    end_time = "11:00:00"
-
-    item = {
-    'UserID': user_id,
-    'EventID': event_id,
-    'Date': date,
-    'StartTime': start_time,
-    'EndTime': end_time
-    }
-
-    table.put_item(Item = item)
-    print("Added successfully")
+    print("FIX")
 
 
 
 
 if __name__ == "__main__":
+    data = {"name": "Los Angeles", "state": "CA", "country": "USA"}
 
-    aws_access_key_id, aws_secret_access_key = read_aws_creds(csv_file_path)
-
-
-    session = boto3.Session(
-        aws_access_key_id = aws_access_key_id,
-        aws_secret_access_key = aws_secret_access_key,
-        region_name = 'us-east-2'
-    )
-
-    dynamodb = session.resource('dynamodb')
-
-    table = dynamodb.Table('Calendar')
-
-    try:
-        table_list = list(dynamodb.tables.all())
-        print("Tables in DynamoDB:")
-        for table in table_list:
-            print(table.name)
-
-    except Exception as e:
-        print(f"Error: {e}")
-
-    addEvent()
+    db.collection("cities").document("LA").set(data)
